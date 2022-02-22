2e83498317f10b60b65bca2754777508c49c2585
Thyago de Oliveira Perez <thyagodoliveiraperez@gmail.com>
2022-02-22 17:12:09 -0300

    inclusao da pasta de arquivos que contem a API, API ainda nao foi finalizada

---
+++ API/API_meu_login/API_meu_login/Controllers/ApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace API_meu_login.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        public static IWebHostEnvironment _environment;
+        public ApiController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        [HttpGet("login")]
+        public string Get()
+        {
+            string texto = " Web API - ImagemController em execução : " + DateTime.Now.ToLongTimeString();
+            texto += $"\n Ambiente :  {_environment.EnvironmentName}";
+            return texto;
+        }
+
+        [HttpPost("cadastro")]
+        public async Task<IActionResult> Post(IFormFile json)
+        {
+            if (json != null)
+            {
+                return Ok(json.FileName);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
